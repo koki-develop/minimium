3a7e128ca3291fe0c9f95b5c0ae4f6bd2baf219d
koki-develop <kou.pg.0131@gmail.com>
2025-01-20 15:27:13 +0900

    Enable react compiler

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
+});
