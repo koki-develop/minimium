6d0ecd013a2ab5a192baf8202bbf2505d1dfe0b3
koki-develop <kou.pg.0131@gmail.com>
2025-01-21 14:01:25 +0900

    Refactor AddressBar component: add input reference for improved focus management on form submission

--- src/renderer/components/AddressBar/AddressBar.tsx
+++ src/renderer/components/AddressBar/AddressBar.tsx
@@ -1,5 +1,5 @@
 import { TextInput } from "@mantine/core";
-import type { FormEvent } from "react";
+import { type FormEvent, useRef } from "react";
 
 export type AddressBarProps = {
   query: string;
@@ -12,15 +12,19 @@ export default function AddressBar({
   onChange,
   onSubmit,
 }: AddressBarProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(query);
+    inputRef.current?.blur();
   };
 
   return (
     <div className="border-gray-200 border-b border-solid px-4 py-2">
       <form onSubmit={handleSubmit}>
         <TextInput
+          ref={inputRef}
           className="w-full"
           radius="xl"
           type="text"
