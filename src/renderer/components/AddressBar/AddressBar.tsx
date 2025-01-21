3bd9788ddd68213eccc19eaa501f9b7631e4fd87
koki-develop <kou.pg.0131@gmail.com>
2025-01-21 15:33:38 +0900

    Refactor AddressBar and App components: Enhance AddressBar with navigation controls and loading state, update App to manage webview state and integrate AddressBar for improved URL handling and user interaction.

--- src/renderer/components/AddressBar/AddressBar.tsx
+++ src/renderer/components/AddressBar/AddressBar.tsx
@@ -1,15 +1,37 @@
-import { TextInput } from "@mantine/core";
+import { ActionIcon, TextInput } from "@mantine/core";
+import {
+  IconArrowLeft,
+  IconArrowRight,
+  IconProgressX,
+  IconRefresh,
+} from "@tabler/icons-react";
 import { type FormEvent, useRef } from "react";
 
 export type AddressBarProps = {
   query: string;
-  onChange: (query: string) => void;
+  loading: boolean;
+  canGoBack: boolean;
+  canGoForward: boolean;
+
+  onQueryChange: (query: string) => void;
+  onGoBack: () => void;
+  onGoForward: () => void;
+  onRefresh: () => void;
+  onStop: () => void;
   onSubmit: (query: string) => void;
 };
 
 export default function AddressBar({
   query,
-  onChange,
+  loading,
+  canGoBack,
+  canGoForward,
+
+  onQueryChange,
+  onGoBack,
+  onGoForward,
+  onRefresh,
+  onStop,
   onSubmit,
 }: AddressBarProps) {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -21,15 +43,46 @@ export default function AddressBar({
   };
 
   return (
-    <div className="border-gray-200 border-b border-solid px-4 py-2">
-      <form onSubmit={handleSubmit}>
+    <div className="flex items-center gap-2 border-gray-200 border-b border-solid px-4 py-2">
+      <ActionIcon
+        classNames={{ root: "disabled:bg-transparent" }}
+        color="dark"
+        variant="subtle"
+        radius="xl"
+        onClick={onGoBack}
+        disabled={!canGoBack}
+      >
+        <IconArrowLeft size={18} />
+      </ActionIcon>
+
+      <ActionIcon
+        classNames={{ root: "disabled:bg-transparent" }}
+        color="dark"
+        variant="subtle"
+        radius="xl"
+        onClick={onGoForward}
+        disabled={!canGoForward}
+      >
+        <IconArrowRight size={18} />
+      </ActionIcon>
+
+      <ActionIcon
+        classNames={{ root: "disabled:bg-transparent" }}
+        color="dark"
+        variant="subtle"
+        radius="xl"
+        onClick={loading ? onStop : onRefresh}
+      >
+        {loading ? <IconProgressX size={18} /> : <IconRefresh size={18} />}
+      </ActionIcon>
+
+      <form className="flex-grow" onSubmit={handleSubmit}>
         <TextInput
           ref={inputRef}
-          className="w-full"
           radius="xl"
           type="text"
           value={query}
-          onChange={(e) => onChange(e.currentTarget.value)}
+          onChange={(e) => onQueryChange(e.currentTarget.value)}
         />
       </form>
     </div>
--- src/renderer/components/App/App.tsx
+++ src/renderer/components/App/App.tsx
@@ -1,21 +1,45 @@
 import { useRef, useState } from "react";
 import AddressBar from "../AddressBar";
-import Webview, { type WebviewTag } from "../Webview";
+import Webview, { type WebviewState, type WebviewTag } from "../Webview";
 
 export default function App() {
   const webviewRef = useRef<WebviewTag>(null);
-  const [webviewUrl, setWebviewUrl] = useState("https://duckduckgo.com");
-  const [url, setUrl] = useState("https://duckduckgo.com");
+  const [webviewUrl, setWebviewUrl] = useState<string>(
+    "https://duckduckgo.com",
+  );
+  const [webviewState, setWebviewState] = useState<WebviewState | null>(null);
+
+  const [query, setQuery] = useState<string>("");
+
+  const handleWebviewStateChange = (state: WebviewState) => {
+    setWebviewState(state);
+    setQuery(state.url);
+  };
+
+  const handleSubmit = (query: string) => {
+    setWebviewUrl(query);
+  };
 
   return (
     <div className="flex min-h-dvh min-w-dvw flex-col">
-      <AddressBar query={url} onChange={setUrl} onSubmit={setWebviewUrl} />
+      <AddressBar
+        query={query}
+        loading={webviewState?.isLoading ?? false}
+        canGoBack={webviewState?.canGoBack ?? false}
+        canGoForward={webviewState?.canGoForward ?? false}
+        onQueryChange={(query) => setQuery(query)}
+        onGoBack={() => webviewRef.current?.goBack()}
+        onGoForward={() => webviewRef.current?.goForward()}
+        onRefresh={() => webviewRef.current?.reload()}
+        onStop={() => webviewRef.current?.stop()}
+        onSubmit={handleSubmit}
+      />
 
       <Webview
         ref={webviewRef}
         className="flex-grow"
         src={webviewUrl}
-        onUrlChange={(url) => setUrl(url)}
+        onStateChange={(state) => handleWebviewStateChange(state)}
       />
     </div>
   );
