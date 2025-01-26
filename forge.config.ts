279a69e996133cea06270dc0b6b6e29cf3e4099d
koki-develop <kou.pg.0131@gmail.com>
2025-01-26 14:17:36 +0900

    Minimium -> minimium

--- forge.config.ts
+++ forge.config.ts
@@ -12,7 +12,7 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     icon: "assets/icon",
-    executableName: "Minimium",
+    executableName: "minimium",
   },
   rebuildConfig: {},
   makers: [
