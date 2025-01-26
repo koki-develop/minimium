1611d50c2e2929ccd99d8a8b5406b9a0ee3b8994
koki-develop <kou.pg.0131@gmail.com>
2025-01-26 14:05:52 +0900

    Configure GitHub publisher for Electron Forge

--- forge.config.ts
+++ forge.config.ts
@@ -4,6 +4,7 @@ import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { VitePlugin } from "@electron-forge/plugin-vite";
+import { PublisherGithub } from "@electron-forge/publisher-github";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
 
@@ -18,6 +19,14 @@ const config: ForgeConfig = {
     new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
+  ],
+  publishers: [
+    new PublisherGithub({
+      repository: {
+        owner: "koki-develop",
+        name: "minimium",
+      },
+    }),
   ],
   plugins: [
     new VitePlugin({
